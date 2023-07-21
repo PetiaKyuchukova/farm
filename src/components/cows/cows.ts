775ee70aaff2aaa91114ace87dd269d58771c83b
Petya Kyuchukova <petia.kiuchukova@abv.bg>
2023-07-21 21:34:15 +0300

    search bar

--- src/components/cows/cows.ts
+++ src/components/cows/cows.ts
@@ -1,5 +1,5 @@
-import { customElement, property} from 'lit/decorators.js'
-import {LitElement, html, css} from 'lit'
+import { customElement, property,query,state} from 'lit/decorators.js'
+import {LitElement, html, css, TemplateResult, nothing} from 'lit'
 import {Cow} from "./cow.type.ts";
 
 @customElement('farm-herd')
@@ -40,6 +40,16 @@ export class FarmHerd extends LitElement {
     @property({attribute: false, type: Boolean})
     isLoading = false
 
+    @query("#myInput")
+    myInput: HTMLInputElement
+
+    @query("#myTable")
+    myTable: HTMLElement
+
+
+    @state()
+    up:any[] = []
+
     private fetchData() {
         this.updateComplete.then(() => {
             this.isLoading = true
@@ -59,6 +69,7 @@ export class FarmHerd extends LitElement {
                     this.isLoading = false
                 })
         })
+
     }
 
 
@@ -66,7 +77,29 @@ export class FarmHerd extends LitElement {
         this.fetchData()
     }
 
-    render() {
+    private myFunction(e: any) {
+
+        var filter, table, tr, td, i,j, txtValue;
+        filter = e.target.value.toUpperCase();
+        table = this.myTable
+        tr = table.getElementsByTagName("tr");
+
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td");
+            for (j = 0; j < td.length; j++) {
+                if (td[j]) {
+                    txtValue = td[j].textContent || td[j].innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                        break
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    }
+    private renderTable(){
         let rows = []
         if (this.data!=undefined){
             for (const cow of this.data) {
@@ -82,13 +115,19 @@ export class FarmHerd extends LitElement {
             }
         }
 
+        return rows
+    }
+
+    render() {
+
+        let rows = this.renderTable()
 
         return html`
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
             
-            <div class="search"> 
-                <h2>Search</h2>
+            <div class="search">
+                <input type="text" id="myInput" @keyup="${this.myFunction}" placeholder="Search for cows.." title="Type in a name">
             </div>
             <div class="content">
                 <div style="display: flex;     justify-content: space-between;">
@@ -96,12 +135,14 @@ export class FarmHerd extends LitElement {
                     <button type="button" style="    height: 40px;" class="btn btn-success">+ Add cow</button>
                     
                 </div>
-                <table class="table table-hover">
+                <table id="myTable" class="table table-hover">
                     <thead>
-                        <td>Cow ID</td>
-                        <td>Gender</td>
-                        <td>Breed</td>
-                        <td>Color</td>
+                    <tr>
+                        <th>Cow ID</th>
+                        <th>Gender</th>
+                        <th>Breed</th>
+                        <th>Color</th>
+                    </tr>
                     </thead>
                     <tbody>
                         ${rows}
